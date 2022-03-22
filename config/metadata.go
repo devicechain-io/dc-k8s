a5ae0b7523b695ca43a005a23d81fa8c65a97389
Derek Adams <dadams@figure.com>
2022-03-22 16:39:04 -0400

    working on rbac and operator manifest

--- config/metadata.go
+++ config/metadata.go
@@ -3,8 +3,22 @@ package config
 import "embed"
 
 //go:embed crd/bases/*
-var manifests embed.FS
+var crds embed.FS
 
-func Manifests() embed.FS {
-	return manifests
+func CrdFiles() embed.FS {
+	return crds
+}
+
+//go:embed rbac/*
+var rbac embed.FS
+
+func RbacFiles() embed.FS {
+	return rbac
+}
+
+//go:embed manager/*
+var manager embed.FS
+
+func ManagerFiles() embed.FS {
+	return manager
 }
--- main.go
+++ main.go
@@ -40,13 +40,9 @@ func init() {
 
 func main() {
 	var metricsAddr string
-	var enableLeaderElection bool
 	var probeAddr string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
 	opts := zap.Options{
 		Development: true,
 	}
@@ -60,7 +56,7 @@ func main() {
 		MetricsBindAddress:     metricsAddr,
 		Port:                   9443,
 		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
+		LeaderElection:         false,
 		LeaderElectionID:       "27be0819.devicechain.io",
 	})
 	if err != nil {
